2171d6d195da0ebf954c3b55e7e806fdcd2d0ead
Oliver Chard [el16ohc] <el16ohc@eentpc05.leeds.ac.uk>
2017-11-17 16:35:45 +0000

    Initial Commit

---
+++ dangerZone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dangerZone
+//
+//  Created by Oliver Chard [el16ohc] on 17/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dangerZone/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  dangerZone
+//
+//  Created by Oliver Chard [el16ohc] on 17/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
