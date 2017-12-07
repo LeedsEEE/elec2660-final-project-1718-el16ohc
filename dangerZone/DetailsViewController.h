a66c17e0d1960e86dedfa00f0f5ac1e7f5abd18e
el16ohc <33663084+el16ohc@users.noreply.github.com>
2017-12-07 18:55:27 +0000

    Update DetailsViewController.h

--- dangerZone/DetailsViewController.h
+++ dangerZone/DetailsViewController.h
@@ -10,7 +10,7 @@
 #import "equipment.h"
 
 @interface DetailsViewController : UIViewController
-// Outlets for all Labels and UIImageView.
+// Defines outlets for all Labels and UIImageView.
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *calibreLabel;
 @property (weak, nonatomic) IBOutlet UILabel *ammoLabel;
