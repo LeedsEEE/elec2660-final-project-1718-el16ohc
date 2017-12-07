3aaf188d6f03531e7c6b9b9a62b798acfc3cec9e
el16ohc <33663084+el16ohc@users.noreply.github.com>
2017-12-07 18:35:07 +0000

    Update TableViewController.m

--- dangerZone/TableViewController.m
+++ dangerZone/TableViewController.m
@@ -38,12 +38,7 @@
     return cell;
 }
 #pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    
     if ([[segue identifier] isEqualToString:@"showEquipmentDetails"]) {
         DetailsViewController *destinationViewController = [segue destinationViewController];
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
